b02bc4206e2241fe3bd50b6ae8e17d2bb3f2d761
ibrahimerian28-coder <ibrahimerian28@gmail.com>
2026-04-30 22:42:55 +0300

    Update app.py

--- app.py
+++ app.py
@@ -60,7 +60,7 @@ def read_gsheet(sheet_name):
     return load_data(gids.get(sheet_name, "0"))
     def calculate_day_cogs(target_date):
     """حساب تكلفة الشمعات المستخدمة في تاريخ معين"""
-    if df_m.empty or df_inv.empty:
+        if df_m.empty or df_inv.empty:
         return 0
     # فلترة الصيانات التي تمت في هذا التاريخ
     day_maint = df_m[df_m['v_date_dt'].dt.date == target_date]
